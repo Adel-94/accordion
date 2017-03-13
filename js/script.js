710bf015c5e7278519986202fb385731f9684b23
Adel-94 <A.Ismayilova94@hotmail.com>
2017-03-13 11:00:08 +0400

    fdksjhk

--- js/script.js
+++ js/script.js
@@ -2,28 +2,41 @@ function but1() {
 document.getElementsByClassName("sect")[0].style.display ='block';
 document.getElementsByClassName("but")[0]  .style.background = 'blue';
 document.getElementsByClassName("sect")[1].style.display ='none';
+document.getElementsByClassName("but")[1].style.background ='lightgray';
 document.getElementsByClassName("sect")[2].style.display ='none';
+document.getElementsByClassName("but")[2].style.background ='lightgray';
 document.getElementsByClassName("sect")[3].style.display ='none';
+document.getElementsByClassName("but")[3].style.background ='lightgray';
+
 }
 
 function but2() {
 document.getElementsByClassName("sect")[0].style.display ='none';
 document.getElementsByClassName("sect")[1].style.display ='block';
+document.getElementsByClassName("but")[0]  .style.background = 'lightgray';
 document.getElementsByClassName("but")[1]  .style.background = 'blue';
 document.getElementsByClassName("sect")[2].style.display ='none';
+document.getElementsByClassName("but")[2]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[3].style.display ='none';
+document.getElementsByClassName("but")[3]  .style.background = 'lightgray';
 }
 function but3() {
 document.getElementsByClassName("sect")[0].style.display ='none';
+document.getElementsByClassName("but")[0]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[1].style.display ='none';
+document.getElementsByClassName("but")[1]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[2].style.display ='block';
 document.getElementsByClassName("but")[2]  .style.background = 'blue';
 document.getElementsByClassName("sect")[3].style.display ='none';
+document.getElementsByClassName("but")[3]  .style.background = 'lightgray';
 }
 function but4() {
 document.getElementsByClassName("sect")[0].style.display ='none';
+document.getElementsByClassName("but")[0]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[1].style.display ='none';
+document.getElementsByClassName("but")[1]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[2].style.display ='none';
+document.getElementsByClassName("but")[2]  .style.background = 'lightgray';
 document.getElementsByClassName("sect")[3].style.display ='block';
 document.getElementsByClassName("but")[3]  .style.background = 'blue';
 }
